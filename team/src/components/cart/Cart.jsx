ec9b6aca3c5b493f74364eb25240a7214ab8cc10
Muslimbek610 <muslimbekrahmatullayev3@gmail.com>
2023-02-20 15:27:47 +0500

    img

--- team/src/components/cart/Cart.jsx
+++ team/src/components/cart/Cart.jsx
@@ -1,12 +1,20 @@
 import "../cart/Cart.css";
-
+import Img from "./photo.jpg";
 function Cart() {
   return (
     <div className="carts">
-      <div className="cart_cart">salom</div>
-      <div className="cart_cart">salom</div>
-      <div className="cart_cart">salom</div>
-      <div className="cart_cart">salom</div>
+      <div className="cart_cart">
+        <img src={Img} alt="" className="img" />
+      </div>
+      <div className="cart_cart">
+        <img src={Img} alt="" className="img" />
+      </div>
+      <div className="cart_cart">
+        <img src={Img} alt="" className="img" />
+      </div>
+      <div className="cart_cart">
+        <img src={Img} alt="" className="img" />
+      </div>
     </div>
   );
 }
